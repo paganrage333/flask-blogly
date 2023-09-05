76ba616a30ee9a36566c100da57a6f33c251ae5b
paganrage333 <aragan@unca.edu>
2023-09-05 12:19:13 -0400

    added secret key

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from models import db, connect_db, User, Post
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG'] = True
 app.app_context().push()
