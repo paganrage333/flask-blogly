e77e4b5c3abafd5cda6acaef00948e120a1fe1e1
paganrage333 <aragan@unca.edu>
2023-09-06 13:06:31 -0400

    routes

--- app.py
+++ app.py
@@ -142,3 +142,58 @@ def tag_index():
     tags = Tag.query.all()
     return render_template('tags/index.html', tags=tags)
 
+@app.route('/tags/new')
+def tags_new_form():
+
+    posts = Post.query.all()
+    return render_template('tags/new.html', posts=posts)
+
+@app.route("/tags/new", methods=["POST"])
+def tags_new():
+
+    post_ids = [int(num) for num in request.form.getlist("posts")]
+    posts = Post.query.filter(Post.id.in_(post_ids)).all()
+    new_tag = Tag(name=request.form['name'], posts=posts)
+
+    db.session.add(new_tag)
+    db.session.commit()
+    flash(f"Tag '{new_tag.name}' added.")
+
+    return redirect("/tags")
+
+@app.route('/tags/<int:tag_id>')
+def show_tags(tag_id):
+
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('tags/show.html', tag=tag)
+
+@app.route('/tags/<int:tag_id>/edit')
+def tags_edit_form(tag_id):
+
+    tag = Tag.query.get_or_404(tag_id)
+    posts = Post.query.all()
+    return render_template('tags/edit.html', tag=tag, posts=posts)
+
+@app.route('/tags/<int:tag_id>/edit', methods=["POST"])
+def tags_edit(tag_id):
+
+    tag = Tag.query.get_or_404(tag_id)
+    tag.name = request.form['name']
+    post_ids = [int(num) for num in request.form.getlist("posts")]
+    tag.posts = Post.query.filter(Post.id.in_(post_ids)).all()
+
+    db.session.add(tag)
+    db.session.commit()
+    flash(f"Tag '{tag.name}' edited.")
+
+    return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>/delete', methods=["POST"])
+def tags_delete(tag_id):
+
+    tag = Tag.query.get_or_404(tag_id)
+    db.session.delete(tag)
+    db.session.commit()
+    flash(f"Tag '{tag.name}' deleted.")
+
+    return redirect('/tags')
